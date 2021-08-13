89caed85ce58da8932a78d30ccf9ea3531b36809
Cassandra <cparisi1290@gmail.com>
2021-08-13 13:49:03 -0500

    import menu icon to header

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import MenuSharp from '@material-ui/icons/MenuSharp';
 
 function Header() {
   return (
     <div className="header">
       <h1>Header</h1>
+      <MenuSharp />
     </div>
   );
 }
