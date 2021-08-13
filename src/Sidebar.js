cc221c32f997ef1198028508e81125a75e557314
Cassandra <cparisi1290@gmail.com>
2021-08-13 16:38:33 -0500

    send icons down as props to sidebar row component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './sidebar.css';
 import SidebarRow from './SidebarRow';
+import HomeIcon from '@material-ui/icons/Home';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
-      <SidebarRow title="Home" />
-      <SidebarRow title="Trending" />
-      <SidebarRow title="Subscription" />
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={WhatshotIcon} title="Trending" />
+      <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
     </div>
   );
 }
