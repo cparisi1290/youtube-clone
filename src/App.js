1e69a44371582f4065a09384396bd395d38cba81
Cassandra <cparisi1290@gmail.com>
2021-08-13 14:03:52 -0500

    import youtube header icons and youtube logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './Header';
 function App() {
   return (
     <div className="app">
-      <h1>Youtube Clone</h1>
       <Header />
       {/* Sidebar */}
       {/* Recommended Videos */}
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import MenuSharp from '@material-ui/icons/MenuSharp';
+import SearchSharp from '@material-ui/icons/SearchSharp';
+import VideoCallSharp from '@material-ui/icons/VideoCallSharp';
+import AppsSharp from '@material-ui/icons/AppsSharp';
+import NotificationsSharp from '@material-ui/icons/NotificationsSharp';
+import Avatar from '@material-ui/core/Avatar';
 
 function Header() {
   return (
     <div className="header">
-      <h1>Header</h1>
       <MenuSharp />
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+        alt="YouTube Logo"
+        className="header__logo"
+      />
+      <input type="text" />
+      <SearchSharp />
+      <VideoCallSharp />
+      <AppsSharp />
+      <NotificationsSharp />
+      <Avatar />
     </div>
   );
 }
