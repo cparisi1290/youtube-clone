1d485773c58de74d643ebec6acd83ff6ae4f20b3
Cassandra <cparisi1290@gmail.com>
2021-08-13 16:23:51 -0500

    import sidebar row compoonent with 3 psuedo codes to start

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './sidebar.css';
+import SidebarRow from './SidebarRow';
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <h1>Sidebar</h1>
+      <SidebarRow title="Home" />
+      <SidebarRow title="Trending" />
+      <SidebarRow title="Subscription" />
     </div>
   );
 }
