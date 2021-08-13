32f7c44e9b05a38560be85411d1294c5916847f5
Cassandra <cparisi1290@gmail.com>
2021-08-13 13:04:44 -0500

    add firebase configure code

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB9qOFnGm_7v_lx3wP_iOMkdp9D6jahIqI',
+  authDomain: 'clone-d1f63.firebaseapp.com',
+  projectId: 'clone-d1f63',
+  storageBucket: 'clone-d1f63.appspot.com',
+  messagingSenderId: '931666580848',
+  appId: '1:931666580848:web:4393ebf3c57d608e52ebb5',
+};
