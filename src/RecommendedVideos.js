1344be3aa0743a50ea9b999f7951dc434f21a799
Cassandra <cparisi1290@gmail.com>
2021-09-13 13:13:39 -0500

    add video component to container

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './recommendedVideos.css';
+import VideoCard from './VideoCard';
 
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
-      <h1>Recommended Videos</h1>
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+      </div>
     </div>
   );
 }
