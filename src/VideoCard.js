7eb7943e6a465bd4ef3e768b7177b65c1b7d61b3
Cassandra <cparisi1290@gmail.com>
2021-09-13 14:33:42 -0500

    corrected className

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -6,14 +6,19 @@ function VideoCard({ image, title, channel, views, timestamp, channelImage }) {
   return (
     <div className="videoCard">
       <img className="videoCard__thumbnail" src={image} alt="" />
-      <div className="video__info">
-        <Avatar className="videoCard__avatar" alt={channel} src={Avatar} />
-        <div className="video__text"></div>
-        <h4>{title}</h4>
-        <p>{channel}</p>
-        <p>
-          {views} • {timestamp}
-        </p>
+      <div className="videoCard__info">
+        <Avatar
+          className="videoCard__avatar"
+          alt={channel}
+          src={channelImage}
+        />
+        <div className="videoCard__text">
+          <h4>{title}</h4>
+          <p>{channel}</p>
+          <p>
+            {views} • {timestamp}
+          </p>
+        </div>
       </div>
     </div>
   );
