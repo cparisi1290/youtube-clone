3a1837e3f4d9de1184c87fb5d2e85861b5b9676f
Cassandra <cparisi1290@gmail.com>
2021-09-13 14:04:58 -0500

    add dummy data for styling

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -7,7 +7,14 @@ function RecommendedVideos() {
     <div className="recommendedVideos">
       <h2>Recommended</h2>
       <div className="recommendedVideos__videos">
-        <VideoCard />
+        <VideoCard
+          title="Become a Developer"
+          views="2.5M Views"
+          timestamp="3 days ago"
+          channelImage="https://cdn.sanity.io/images/81pocpw8/production/43075765fea446689208becf6947fcf497d621fc-4032x3024.jpg?rect=1007,0,3024,3024&w=320&h=320&fit=crop&auto=format"
+          channel="Cassandra Parisi"
+          image="https://miro.medium.com/max/900/0*_kzCaYRrAjcxUoHA.jpg"
+        />
         <VideoCard />
         <VideoCard />
         <VideoCard />
