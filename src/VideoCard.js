2c9b63fcc5952dafba1b0fee7ea53c2a1215714b
Cassandra <cparisi1290@gmail.com>
2021-09-13 14:09:53 -0500

    add dummy data to use for styling

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import './videoCard.css';
 
 function VideoCard({ image, title, channel, views, timestamp, channelImage }) {
   return (
-    <div>
-      <img src={image} alt="" />
-      <div className="video__info"></div>
+    <div className="videoCard">
+      <img className="videoCard__thumbnail" src={image} alt="" />
+      <div className="video__info">
+        <Avatar className="videoCard__avatar" alt={channel} src={Avatar} />
+        <div className="video__text"></div>
+        <h4>{title}</h4>
+        <p>{channel}</p>
+        <p>
+          {views} • {timestamp}
+        </p>
+      </div>
     </div>
   );
 }
